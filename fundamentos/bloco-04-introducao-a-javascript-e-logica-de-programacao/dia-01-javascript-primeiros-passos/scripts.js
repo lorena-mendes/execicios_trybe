22b34f19c2c110e53c27ead1b3fec2614571b651
Lorena Mendes <lorena.lccma@gmail.com>
2022-05-12 19:45:02 -0300

    Fiz as operações aritméticas

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
@@ -10,3 +10,19 @@ console.log(typeof patientId);
 console.log(typeof isEnrolled);
 console.log(typeof patientInfo);
 console.log(typeof patientEmail);
+
+//Crie uma constante chamada base e uma variável chamada height e atribua os seus respectivos valores: 5 e 8.
+
+const base = 5;
+const height = 8;
+
+//Crie uma constante chamada area e atribua a ela o resultado da multiplicação da base pela altura.
+
+const area = base * height;
+console.log(area);
+
+//Crie uma constante chamada perimeter e atribua a ela a soma de todos os lados do retângulo.
+
+const perimeter = 5 + 5 + 8 + 8;
+console.log(perimeter);
+
