a1597c5b80957409cb499fd934423daa4ab9fa35
Lorena Mendes <lorena.lccma@gmail.com>
2022-05-12 19:09:26 -0300

    Atribui novo valor à constante

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
@@ -1,6 +1,6 @@
 const myName = "Lorena";
-const birthCity = "Belo Horizonte";
-let birthYear = "1991";
+const birthCity = "Belo Horizonte"; birthCity = "Sabará";
+let birthYear = "1991"; birthYear = "2030";
 
 console.log(myName);
 console.log(birthCity);
