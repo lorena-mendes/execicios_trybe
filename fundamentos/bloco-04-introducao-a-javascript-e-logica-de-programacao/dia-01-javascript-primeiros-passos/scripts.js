e64e164e5d908913720b4eaa6cc88c500440bf3f
Lorena Mendes <lorena.lccma@gmail.com>
2022-05-12 20:24:35 -0300

    Exercícios condições if e else

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
@@ -1,28 +1,19 @@
-let patientId = 50;
-let isEnrolled = true;
-const patientInfo = {
-  firstName: 'Ana',
-  lastName: 'Santos',
-};
-const patientEmail = 'ana@email.com';
+//1. Crie uma constante que receba a nota de uma pessoa candidata em um desafio técnico, e atribua a ela um valor entre 1 e 100
 
-console.log(typeof patientId);
-console.log(typeof isEnrolled);
-console.log(typeof patientInfo);
-console.log(typeof patientEmail);
+const nota = 80;
 
-//Crie uma constante chamada base e uma variável chamada height e atribua os seus respectivos valores: 5 e 8.
+//2. Implemente uma lógica que verifique se a pessoa candidata foi aprovada, reprovada ou se ela está na lista de espera. Para isso, considere as seguintes informações:
+//  Se a nota for maior ou igual a 80, imprima "Parabéns, você foi aprovada(o)!"
+//  Se a nota for menor que 80 e maior ou igual a 60, imprima "Você está na nossa lista de espera"
+//  Se a nota for menor que 60, imprima "Você foi reprovada(o)"
 
-const base = 5;
-const height = 8;
-
-//Crie uma constante chamada area e atribua a ela o resultado da multiplicação da base pela altura.
-
-const area = base * height;
-console.log(area);
-
-//Crie uma constante chamada perimeter e atribua a ela a soma de todos os lados do retângulo.
-
-const perimeter = 5 + 5 + 8 + 8;
-console.log(perimeter);
+if (nota >= 80) {
+    console.log("Parabéns, você foi aprovada(o)!")
+}
+else if (nota < 80 && nota >= 60) {
+    console.log("Você está na nossa lista de espera.")
+}
+else {
+    console.log("Você foi reprovada(o).")
+}
 
