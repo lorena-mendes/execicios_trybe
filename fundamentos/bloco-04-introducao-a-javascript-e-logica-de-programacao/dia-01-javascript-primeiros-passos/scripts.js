7858c8179537092a7ef1a281b03ff7257b9ae009
Lorena Mendes <lorena.lccma@gmail.com>
2022-05-13 15:58:05 -0300

    Fiz o programa para identificar se os ângulos eram de um triângulo

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
@@ -1,4 +1,4 @@
-//Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
+//1. Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
 //Adição (a + b)
 //Subtração (a - b)
 //Multiplicação (a * b)
@@ -14,7 +14,7 @@ console.log(a * b);
 console.log(a / b);
 console.log(a % b);
 
-//Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+//2. Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
 
 const x = 19;
 const y = 35;
@@ -25,7 +25,7 @@ if (x > y) {
     console.log("y é maior que x")
 }
 
-//Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
+//3. Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
 
 const h = 96;
 const i = 14;
@@ -39,7 +39,7 @@ if (h > i && h > c) {
     console.log("c é o maior número")
  };
 
- //Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+ //4. Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
 
  const valor = -34;
 
@@ -50,3 +50,23 @@ if (h > i && h > c) {
  } else {
      console.log("Zero")
  };
+
+ //5. Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+
+ const m = 45;
+ const n = 52;
+ const p = 83;
+
+ const somaDosAngulos = m + n + p;
+ const valorDosAngulos = m > 0 && n > 0 && p>0;
+
+ if (valorDosAngulos) {
+     if (somaDosAngulos === 180) {
+         console.log("true")
+     } else {
+         console.log("false")
+     }
+ } else {
+    console.log("Ângulo inválido")
+ };
+
