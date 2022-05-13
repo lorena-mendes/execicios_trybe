c3048f2e7f231440c1e355d7725e361b21f36457
Lorena Mendes <lorena.lccma@gmail.com>
2022-05-13 15:16:07 -0300

    Fiz o programa para retonar o maior número

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
@@ -13,3 +13,14 @@ console.log(a - b);
 console.log(a * b);
 console.log(a / b);
 console.log(a % b);
+
+//Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+
+const x = 19;
+const y = 35;
+
+if (x > y) {
+    console.log("x é maior que y")
+} else {
+    console.log("y é maior que x")
+}
