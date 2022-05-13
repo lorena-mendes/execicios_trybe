59e888308fbecbfefe5324395481e28042da0fb7
Lorena Mendes <lorena.lccma@gmail.com>
2022-05-13 13:22:13 -0300

    Mandei imprimir os tipos das variáveis

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
@@ -1,19 +1,13 @@
-//1. Crie uma constante que receba a nota de uma pessoa candidata em um desafio técnico, e atribua a ela um valor entre 1 e 100
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
 
-const nota = 80;
-
-//2. Implemente uma lógica que verifique se a pessoa candidata foi aprovada, reprovada ou se ela está na lista de espera. Para isso, considere as seguintes informações:
-//  Se a nota for maior ou igual a 80, imprima "Parabéns, você foi aprovada(o)!"
-//  Se a nota for menor que 80 e maior ou igual a 60, imprima "Você está na nossa lista de espera"
-//  Se a nota for menor que 60, imprima "Você foi reprovada(o)"
-
-if (nota >= 80) {
-    console.log("Parabéns, você foi aprovada(o)!")
-}
-else if (nota < 80 && nota >= 60) {
-    console.log("Você está na nossa lista de espera.")
-}
-else {
-    console.log("Você foi reprovada(o).")
-}
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
 
