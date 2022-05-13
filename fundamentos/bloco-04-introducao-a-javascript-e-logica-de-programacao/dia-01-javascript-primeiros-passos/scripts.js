3f6b9e44d1e6252f157f8cce795e62d9c3c10783
Lorena Mendes <lorena.lccma@gmail.com>
2022-05-13 13:49:31 -0300

    Exercícios operador and

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
@@ -1,27 +1,30 @@
-let patientId = 50;
-let isEnrolled = true;
-const patientInfo = {
-  firstName: 'Ana',
-  lastName: 'Santos',
-};
-const patientEmail = 'ana@email.com';
+//Crie uma constante chamada "currentHour" que recebe um número entre 4 e 24 de sua escolha, para representar as horas do dia.
 
-console.log(typeof patientId);
-console.log(typeof isEnrolled);
-console.log(typeof patientInfo);
-console.log(typeof patientEmail);
+const currentHour = 21;
 
-//Crie uma constante chamada base e uma variável chamada height e atribua os seus respectivos valores: 5 e 8.
-const base = 5;
-let height = 8;
+//Crie uma variável chamada "message" que, inicialmente, é uma string vazia.
 
-//Crie uma constante chamada area e atribua a ela o resultado da multiplicação da base pela altura.
+let message = "";
 
-const area = (base*height);
-console.log(area);
+//Implemente condicionais para que:
+//Se o horário for maior ou igual a 22, insira "Não deveríamos comer nada, é hora de dormir" na variável "message".
+//Se o horário for maior ou igual a 18 e menor que 22, insira "Rango da noite, vamos jantar :D" na variável "message".
+//Se o horário for maior ou igual a 14 e menor que 18, insira "Vamos fazer um bolo pro café da tarde?" na variável "message".
+//Se o horário estiver entre 11 e 14, insira "Hora do almoço!!!" na variável "message".
+//Se o horário estiver entre 4 e 11, insira "Hmmm, cheiro de café recém passado" na variável "message".
 
-//Crie uma constante chamada perimeter e atribua a ela a soma de todos os lados do retângulo.
- 
-const perimeter = (5 + 5 + 8 + 8);
-console.log(perimeter);
+if (currentHour >= 22) {
+    console.log("Não deveríamos comer nada, é hora de dormir")
+} else if (currentHour >=18 && currentHour <22) {
+    console.log("Rango da noite, vamos jantar :D")
+} else if (currentHour >=14 && currentHour <18) {
+    console.log("Vamos fazer um bolo pro café da tarde?")
+} else if (currentHour >11 && currentHour <14) {
+    console.log("Hora do almoço!!!")
+} else {
+    console.log("Hmmm, cheiro de café recém passado")
+}
 
+//Agora imprima a variável message fora das suas condições.
+
+console.log(message);
