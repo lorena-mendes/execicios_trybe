1496838656c3e9735a15e52e47808a2bf109e063
Lorena Mendes <lorena.lccma@gmail.com>
2022-05-17 13:53:29 -0300

    Calculei o valor do lucro de um produto

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
@@ -20,3 +20,16 @@ if ((a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0)) {
 } else {
     console.log("false")
 };
+
+//10. Escreva um programa que se inicie com dois valores em duas constantes diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
+
+const custoProduto = 0;
+const valorVenda = 50;
+
+if (custoProduto >= 0 && valorVenda >= 0) {
+    const custoTotal = custoProduto * 1.2;
+    const lucro = (valorVenda - custoTotal)
+    console.log(lucro)
+} else {
+    console.log("Erro!! Valor inválido");
+}
