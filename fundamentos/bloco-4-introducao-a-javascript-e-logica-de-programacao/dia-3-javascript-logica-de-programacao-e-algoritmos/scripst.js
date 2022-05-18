63df078f3f9bc3389571831e6a3cac5832d14bf8
Lorena Mendes <lorena.lccma@gmail.com>
2022-05-18 17:29:53 -0300

    Algoritmo da palavra ao contrário

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripst.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripst.js
@@ -1,6 +1,19 @@
+//O fatorial é a multiplicação de um número natural pelos seus antecessores, exceto o zero.
+//Com base nessas informações, crie um algoritmo que retorne o fatorial de 10.
+
 let fatorial = 1;
 
 for (let index = 10; index > 0; index -= 1) {
     fatorial *= index;
 }
 console.log(fatorial);
+
+//Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para "ananab". Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
+
+let word = 'tryber';
+let contrario = '';
+
+for (let index = 0; index < word.length; index += 1) {
+    contrario += word[word.length - 1 - index];
+}
+console.log(contrario);
