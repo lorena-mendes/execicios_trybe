4e15d21c7a5a99661cfca17ff1cd67459608565f
Lorena Mendes <lorena.lccma@gmail.com>
2022-05-24 10:23:14 -0300

    Exercícios finalizados mais os exercícios bônus

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -81,3 +81,47 @@ console.log(numeros);
 for (let index = 0; index < numeros.length; index += 1) {
     console.log(numeros[index] / 2);
 }
+
+//Exercícios Bônus
+//1. Ordene o array numbers em ordem crescente e imprima seus valores;
+
+let numbers2 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+for (let index = 0; index < numbers2.length; index += 1) {
+    for (let index2 = 0; index2 < index; index2 += 1) {
+        if (numbers2[index] < numbers2[index2]) {
+            let position = numbers2[index];
+            numbers2[index] = numbers2[index2];
+            numbers2[index2] = position;
+        }
+    }
+} 
+console.log(numbers2);
+
+//2. Ordene o array numbers em ordem decrescente e imprima seus valores;
+
+let numbers2 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+for (let index = 0; index < numbers2.length; index += 1) {
+    for (let index2 = 0; index2 < index; index2 += 1) {
+        if (numbers2[index] > numbers2[index2]) {
+            let position = numbers2[index];
+            numbers2[index] = numbers2[index2];
+            numbers2[index2] = position;
+        }
+    }
+} 
+console.log(numbers2);
+
+//3. Agora crie um novo array a partir do array numbers, sem perdê-lo. Cada valor do novo array deverá ser igual ao valor correspondente no array numbers multiplicado pelo seguinte. Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5 (primeiro valor) e 9 (valor seguinte). Já o segundo valor do novo array deverá ser 27, pois é a multiplicação de 9 (segundo valor) e 3 (valor seguinte), e assim por diante. Caso não haja próximo valor, a multiplicação deverá ser feita por 2. Faça isso utilizando o for e o método push. O resultado deve ser o array abaixo:
+
+let numbers2 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let newNumbers = [];
+for (let index = 0; index < numbers2.length; index += 1) {
+    if (index + 1 < numbers2.length) {
+        newNumbers.push(numbers2[index] * numbers2[index + 1]);
+    } else {
+        newNumbers.push(numbers2[index] *2);
+    }
+} 
+console.log(newNumbers);
+
+
