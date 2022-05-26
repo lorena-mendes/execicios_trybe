e063bca7f1f29267d4e311d59ba3b17cbf59dab4
Lorena Mendes <lorena.lccma@gmail.com>
2022-05-26 18:48:21 -0300

    Exercícios parte I - Objetos e For/In

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
@@ -94,7 +94,6 @@ function maiorNumero(num1, num2, num3) {
         return num3;
     };
 };
-
 //4.Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
 
 function resultado(numero) {
@@ -117,9 +116,90 @@ function triangulo(ang1, ang2, ang3) {
         if (soma === 180) {
             return true;
         } else {
-            false;
+           return false;
         };
     } else {
         return 'Erro! Ângulo inválido';
     };
 };
+
+// Exercícios Parte I - Objetos e For/In
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+//1. Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
+
+console.log('Bem vinda, ' + info.personagem);
+
+//2. Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console.
+
+info.recorrente = 'Sim';
+console.log(info);
+
+//3.Faça um for/in que mostre todas as chaves do objeto.
+
+for ( let key in info) {
+    console.log(key);
+};
+
+// //4.Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto.
+
+for ( let key in info) {
+    console.log(info[key]);
+};
+
+//5.Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves.
+
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+  };
+  for (let key in info) {
+    if ( key === 'recorrente' && info[key] === 'Sim' && info2[key] === 'Sim') {
+        console.log('Ambos concorrentes')
+    } else {
+        console.log(info[key] + ' e ' + info2[key]);
+    };
+  };
+  
+
+  //Usando o objeto abaixo, faça os exercícios a seguir:
+  
+  let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+
+  //6.Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
+
+  console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama "' + leitor.livrosFavoritos[0].titulo + '"');
+
+  //7.Adicione um novo livro favorito na chave livrosFavoritos, que é um array. Atribua a esta chave um objeto contendo as seguintes informações:
+
+  leitor.livrosFavoritos.push (
+     {
+        titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+        autor: 'JK Rowling',
+        editora: 'Rocco', 
+  },
+  );
+  console.log(leitor);
+
+//8. Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: "Julia tem 2 livros favoritos".
+
+console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos');
+
