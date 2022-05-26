7ad9b6c645a474ab33921f932b4d4f6389362b8b
Lorena Mendes <lorena.lccma@gmail.com>
2022-05-26 12:31:06 -0300

    Exercícios de fixação - Funções

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
@@ -46,3 +46,80 @@ let names = {
   for (let index in car) {
       console.log(index, car[index]);
   };
+
+  //Refazendo os exercícios de 1 a 5 do dia 4.1
+
+  //1.Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+//Adição (a + b)
+//Subtração (a - b)
+//Multiplicação (a * b)
+//Divisão (a / b)
+//Módulo (a % b)
+
+function soma(a,b) {
+    return a + b;
+};
+function sub(a,b) {
+    return a - b;
+};
+function multi(a,b) {
+    return a * b;
+};
+function divi(a,b) {
+    return a / b;
+};
+function mod(a,b) {
+    return a % b;
+};
+
+//2. Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+
+function bigNumber(number1, number2) {
+    if (number1 > number2) {
+        return number1
+    } else {
+        return number2
+    };
+};
+
+
+//3.Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
+
+function maiorNumero(num1, num2, num3) {
+    if (num1 > num2 && num1 > num3) {
+        return num1;
+    } else if (num2 >num1 && num2 > num3) {
+        return num2;
+    } else {
+        return num3;
+    };
+};
+
+//4.Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+
+function resultado(numero) {
+    if (numero > 0) {
+        return 'Positivo';
+    } else if (numero < 0) {
+        return 'Negativo';
+    } else {
+        return 'Zero';
+    };
+};
+
+//5.Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+
+function triangulo(ang1, ang2, ang3) {
+    let soma = ang1 + ang2 + ang3;
+    let angulosPositivos = ang1 > 0 && ang2 > 0 && ang3 > 0;
+
+    if (angulosPositivos) {
+        if (soma === 180) {
+            return true;
+        } else {
+            false;
+        };
+    } else {
+        return 'Erro! Ângulo inválido';
+    };
+};
