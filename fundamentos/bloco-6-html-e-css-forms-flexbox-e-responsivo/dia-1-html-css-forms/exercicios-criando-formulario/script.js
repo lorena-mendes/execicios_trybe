e84cd286c0bee70b77d3febfd77bd8a70172296c
Lorena Mendes <lorena.lccma@gmail.com>
2022-06-13 18:27:59 -0300

    Adicionei o evento preventDefault.

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicios-criando-formulario/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicios-criando-formulario/script.js
@@ -1,9 +1,5 @@
-let botaoSubmit = document.querySelector('.submit');
+let botaoSubmit = document.getElementById('submit');
 function addEvento(event) {
   event.preventDefault();
 }
 botaoSubmit.addEventListener('click', addEvento);
-
-
-
-
