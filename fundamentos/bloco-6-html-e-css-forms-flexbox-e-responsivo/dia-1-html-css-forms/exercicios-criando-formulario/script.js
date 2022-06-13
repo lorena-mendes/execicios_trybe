82c4e6827df5d627db871a3318375b400f1bde77
Lorena Mendes <lorena.lccma@gmail.com>
2022-06-13 19:55:29 -0300

    Exercicios finalizados.

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicios-criando-formulario/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicios-criando-formulario/script.js
@@ -1,5 +1,31 @@
-let botaoSubmit = document.getElementById('submit');
-function addEvento(event) {
-  event.preventDefault();
-}
-botaoSubmit.addEventListener('click', addEvento);
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const data = new FormData(form);
+  for (const [name, value] of data) {
+    if (name === 'nome' || name === 'email') {
+      if (value.length < 10) {
+        return alert('Dados Inválidos')
+      }
+    }
+    switch (name) {
+      case 'nome':
+        if (value.length > 40) {
+          return alert('Dados Inválidos')
+        }
+        break;
+      case 'email':
+        if (value.length > 50) {
+          return alert('Dados Inválidos')
+        }
+        break;
+      case 'texto':
+        if (value.length > 500) {
+          return alert('Dados Inválidos')
+        }
+        break;
+      default:
+        break;
+    }
+  }
+  return alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip')
+})  
