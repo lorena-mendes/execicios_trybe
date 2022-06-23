e65edc95f6a19df6e099b947a6767e41cdfb8df3
Lorena Mendes <lorena.lccma@gmail.com>
2022-06-23 16:18:04 -0300

    Exercicios para fixar - find -

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios-para-fixar/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios-para-fixar/script.js
@@ -1,4 +1,4 @@
-//. Para fixar
+//. Para fixar forEach
 //1. Use o método forEach chamando a callback showEmailList para apresentar os emails
 
 const emailListInData = [
@@ -13,3 +13,38 @@ const emailListInData = [
   };
   
   emailListInData.forEach(showEmailList);
+
+  // Para fixar find
+  //1. Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5, caso ele exista:
+
+  const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = () => {
+  return numbers.find((number) => number % 3 === 0 && number % 5 === 0)
+}
+
+console.log(findDivisibleBy3And5())
+
+//2. Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista:
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = () => {
+  return names.find((String) => String.length === 5)
+}
+
+console.log(findNameWithFiveLetters());
+
+//3. Utilize o find para encontrar a música com id igual a 31031685, caso ela exista:
+
+const musicas = [
+  { id: '31031685', title: 'Partita in C moll BWV 997' },
+  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+]
+
+function findMusic(id) {
+  return musicas.find((musica) => musica.id === id)
+}
+
+console.log(findMusic('31031685'));
